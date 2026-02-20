8dede4d052e3382562660c545d72f9246a962fd2
Global Name <marieleaertesvaag@outlook.com>
2026-02-20 15:57:03 +0100

    første commit: bash script

---
+++ konsonantklynge.py
@@ -0,0 +1,24 @@
+# requirements
+import nltk, re
+
+
+# import og last ned korpus
+from nltk.corpus import udhr
+from nltk.corpus import words
+
+
+'''finn vokabulæret i korpuset Norwegian_Norsk-Nynorsk-Latin1 frå
+korpuset udhr'''
+
+nynorsk_udhr  = udhr.words('Norwegian_Norsk-Nynorsk-Latin1')
+
+nynorske_ord = sorted(set(nynorsk_udhr))
+
+
+'''finn ord med konsonantklynger med regex'''
+
+
+ccccc_ar_ordpar = [(ccccc, w) for w in nynorske_ord
+                       for ccccc in re.findall(r'[QWRTPSDFGHJKLZXCVBNMqwrtpsdfghjklzxcvbnm]{5}', w)]
+
+print(ccccc_ar_ordpar)
